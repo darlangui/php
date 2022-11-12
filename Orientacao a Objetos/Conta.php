8d1430b3bead3fc2d11c172052677426fe2fc5fb
Darlan <darlaneduardoguimaraes@gmail.com>
2022-11-11 21:12:30 -0300

    transferencia

--- Orientacao a Objetos/Conta.php
+++ Orientacao a Objetos/Conta.php
@@ -3,21 +3,30 @@
 class Conta{
     public string $cpfTitular;  // atributo da conta
     public string $nomeTitular; // atributo da conta
-    public float $saldo; // atributo da conta
+    public float $saldo = 0; // atributo da conta
 
     public function sacar(float $valorSacar){
         if($valorSacar > $this->saldo){
             echo 'Saldo indisponível';
-        }else{
-            $this->saldo -= $valorSacar;
+            return;
         }
+        $this->saldo -= $valorSacar;
     }
 
     public function  depositar(float $valorDeposito){
-        if ($valorDeposito >= 0){
-            $this->saldo += $valorDeposito;
-        }else{
+        if ($valorDeposito < 0) {
             echo `Valor para deposito invalido`;
+            return;
         }
+        $this->saldo += $valorDeposito;
+    }
+
+    public function transferir(float $valorTransferir, Conta $contaDestino){
+        if($valorTransferir > $this->saldo) {
+            echo 'Saldo indisponível';
+            return;
+        }
+        $this->sacar($valorTransferir);
+        $contaDestino->depositar($valorTransferir);
     }
 }
