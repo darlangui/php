73a516b3ffbf656bfdb9cc3112dc7e1c450a85c0
Darlan <darlaneduardoguimaraes@gmail.com>
2022-11-11 21:00:52 -0300

    Saque e Deposito

--- Orientacao a Objetos/Conta.php
+++ Orientacao a Objetos/Conta.php
@@ -4,6 +4,20 @@ class Conta{
     public string $cpfTitular;  // atributo da conta
     public string $nomeTitular; // atributo da conta
     public float $saldo; // atributo da conta
-}
 
-?>
+    public function sacar(float $valorSacar){
+        if($valorSacar > $this->saldo){
+            echo 'Saldo indisponível';
+        }else{
+            $this->saldo -= $valorSacar;
+        }
+    }
+
+    public function  depositar(float $valorDeposito){
+        if ($valorDeposito >= 0){
+            $this->saldo += $valorDeposito;
+        }else{
+            echo `Valor para deposito invalido`;
+        }
+    }
+}
