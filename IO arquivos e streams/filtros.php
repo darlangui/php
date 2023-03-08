f1fdd4bcb027a29a98461a920d38e4233a5941b3
Darlan <darlaneduardoguimaraes@gmail.com>
2023-03-08 10:16:05 -0300

    streams comments

--- IO arquivos e streams/filtros.php
+++ IO arquivos e streams/filtros.php
@@ -7,4 +7,5 @@ $arquivoCursos = fopen('lista-cursos.txt', 'r');
 stream_filter_register('alura.partes', MeuFiltro::class);
 stream_filter_append($arquivoCursos, 'alura.partes');
 
+# fread size of the expansive code
 echo fread($arquivoCursos, filesize('lista-cursos.txt'));
