39987aec81dbfc9cd2552d26b79ba7edcf79fc8f
Darlan <darlaneduardoguimaraes@gmail.com>
2023-03-09 20:53:53 -0300

    spl object

--- IO arquivos e streams/spl.php
+++ IO arquivos e streams/spl.php
@@ -1,1 +1,13 @@
 <?php
+    $arquivoCursos = new SplFileObject('cursos.csv');
+
+    while (!$arquivoCursos->eof()) {
+        $linha = $arquivoCursos->fgetcsv(';');
+
+        echo $linha[0] . PHP_EOL;
+    }
+
+    $date = new DateTime();
+    $date->setTimestamp($arquivoCursos->getCTime());
+
+    echo $date->format('d/m/Y') . PHP_EOL;
