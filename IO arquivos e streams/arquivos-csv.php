08e4d4933f2fec9dcef51d5f101869090edfb402
Darlan <darlaneduardoguimaraes@gmail.com>
2023-03-09 21:40:40 -0300

    csv windowns 1285

--- IO arquivos e streams/arquivos-csv.php
+++ IO arquivos e streams/arquivos-csv.php
@@ -5,13 +5,13 @@
     $arquivoCsv = fopen('cursos.csv', 'w');
 
     foreach ($meusCursos as $curso) {
-        $linha = [trim($curso), 'Sim'];
+        $linha = [trim(utf8_decode($curso)), 'Sim'];
 
         fputcsv($arquivoCsv, $linha, ';');
     }
 
     foreach ($outrosCursos as $curso) {
-        $linha = [trim($curso), 'Não'];
+        $linha = [trim(utf8_decode($curso)), 'Não'];
 
         fputcsv($arquivoCsv, $linha, ';');
     }
